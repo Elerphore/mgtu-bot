a226ae04aa3f885d1973a3182224fdd572591048
elerphore <simon6041@gmail.com>
2020-03-08 16:31:09 +0500

    ewewew

--- downloadAgendaXlsx.rb
+++ downloadAgendaXlsx.rb
@@ -5,22 +5,22 @@ require 'rufus-scheduler'
 ENV['TZ'] = 'Europe/Moscow'
 
 scheduler = Rufus::Scheduler.new
-OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
-$link = 'https://newlms.magtu.ru/pluginfile.php/1160930/mod_folder/content/0/09.03.20 - 11.03.20.xlsx';
-encoded_url = URI.encode($link)
-encoded_pars = URI.parse(encoded_url)
-download = open(encoded_pars);
-IO.copy_stream(download, './changeAgenda.xlsx')
+# $link = 'https://newlms.magtu.ru/pluginfile.php/1160930/mod_folder/content/0/09.03.20 - 11.03.20.xlsx';
+# encoded_url = URI.encode($link)
+# encoded_pars = URI.parse(encoded_url)
+# download = open(encoded_pars);
+# IO.copy_stream(download, './changeAgenda.xlsx')
 
-TimeHours = Time.now.hour;
-TimeMin = Time.now.min;
-# scheduler.cron '5 0 * * *' do
-#   # do something every day, five minutes after midnight
-#   # (see "man 5 crontab" in your terminal)
-# end
+# TimeHours = Time.now.hour;
+# TimeMin = Time.now.min;
+# # scheduler.cron '5 0 * * *' do
+# #   # do something every day, five minutes after midnight
+# #   # (see "man 5 crontab" in your terminal)
+# # end
 
-scheduler.every '5s' do
+scheduler.every '20s' do
 puts('kek')
 end
 
