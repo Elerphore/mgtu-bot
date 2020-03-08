a1f3a10898e17a79e4ad90c789923f50b531af3f
elerphore <simon6041@gmail.com>
2020-03-08 08:52:32 +0500

    add gemfile, minor fix

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'nokogiri'
+gem 'rack', '~> 2.0.1'
+gem 'rspec'
+gem "telegram-bot-ruby"
+gem "roo", "~> 2.8.0"
--- bottg.rb
+++ bottg.rb
@@ -2,19 +2,16 @@ require 'telegram/bot'
 require './parserexcel.rb'
 require './DefaultArrayComponent'
 
+$daySelect = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(Today), %w(Tomorrow)], one_time_keyboard: false);
+$groupSelect = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [$firstGroup.text, $secondGroup.text], one_time_keyboard: false);
 $selectedDay = 1;
+
 token = '1010148951:AAFCVQ9oeZZlPBEvW-_FIPYFicf24wKFg_U'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when '/start'
-        
-
-    	indroduceText = 'select your day'
-    	$daySelect = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [%w(Today), %w(Tomorrow)], one_time_keyboard: false);
-		$groupSelect = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: [$firstGroup.text, $secondGroup.text], one_time_keyboard: false);
-
-    	bot.api.send_message(chat_id: message.chat.id, text: indroduceText, reply_markup: $daySelect)
+    when '/start'        
+    	bot.api.send_message(chat_id: message.chat.id, text: "Select day", reply_markup: $daySelect)
 	when 'Today'
 	    $selectedDay = 1;
     	bot.api.send_message(chat_id: message.chat.id, text: "Select Group", reply_markup: $groupSelect)
