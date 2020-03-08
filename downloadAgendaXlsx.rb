4e63a15417c6c59461a726b9f56cb8bd7c8de7a4
elerphore <simon6041@gmail.com>
2020-03-08 16:49:44 +0500

    ewqeq

--- downloadAgendaXlsx.rb
+++ downloadAgendaXlsx.rb
@@ -19,7 +19,7 @@ scheduler = Rufus::Scheduler.new
 # #   # do something every day, five minutes after midnight
 # #   # (see "man 5 crontab" in your terminal)
 # # end
-
+puts "IS IT WOORK?"
 scheduler.every '1m' do
 puts('kek')
 end
