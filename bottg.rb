71f724a803e7929c58481f3faa60d1a09d4a7379
elerphore <simon6041@gmail.com>
2020-04-08 18:06:08 +0500

    minor fix

--- bottg.rb
+++ bottg.rb
@@ -33,21 +33,21 @@ Telegram::Bot::Client.run(token) do |bot|
 				when 'Сегодня 2 группа'
     			@group = checkExistGroup(bot, message)
 					if ChangeOldFile();
-						if @group != nil && $arrayGroupses.include?(@group)
+						if @group != nil && (@group.kind_of? String)
 							bot.api.send_message(chat_id: message.chat.id, text: "#{funcToday($secondGroup, 1, @group)}", reply_markup: $daySelect, parse_mode: "Markdown")
 						end
 					end
 	    when 'Завтра 1 группа'
     			@group = checkExistGroup(bot, message)
 					if ChangeOldFile();
-						if @group != nil && $arrayGroupses.include?(@group)
+						if @group != nil && (@group.kind_of? String)
 							bot.api.send_message(chat_id: message.chat.id, text: "#{funcToday($firstGroup, 2, @group)}", reply_markup: $daySelect, parse_mode: "Markdown")
 						end
 					end
 	    when 'Завтра 2 группа'
     			@group = checkExistGroup(bot, message)
 					if ChangeOldFile()
-						if @group != nil && $arrayGroupses.include?(@group)
+						if @group != nil && (@group.kind_of? String)
 							bot.api.send_message(chat_id: message.chat.id, text: "#{funcToday($secondGroup, 2, @group)}", reply_markup: $daySelect, parse_mode: "Markdown")
 						end
 					end
