90dfc71d4d373e012cee5fa366c6532b5de2d91e
elerphore <simon6041@gmail.com>
2020-04-12 12:09:26 +0500

    minor fix

--- bot/src/parserexcel.rb
+++ bot/src/parserexcel.rb
@@ -4,10 +4,8 @@ require './bot/src/CheckAgendaDate.rb'
 
 def funcToday(selectedGroup, selectedDay, groupTitle)
 @currNumber = Time.now.strftime("%u").to_i;
-p @currNumber;
 if selectedDay == 1
 	@currNumber = Time.now.strftime("%u").to_i - 1;
 end
-p $arraysWeek[@currNumber];
 	return funcListPars($arraysWeek[@currNumber].title, selectedGroup.id, groupTitle);
 end
