e59d7ab6e5ff64bb3eb2c126df4fcc0aa43fc135
Gness Erquint <gness.na@gmail.com>
2020-04-15 20:11:11 +0300

    Ruby doesn't need semicolons and/or whitespace before an EOL character.

--- bot/src/CheckUserId.rb
+++ bot/src/CheckUserId.rb
@@ -3,50 +3,50 @@ def createArrayGroups
 	$arrayGroupses = []
 	while @id != 573034
 		begin
-		$content = Nokogiri::HTML(open("https://newlms.magtu.ru/mod/folder/view.php?id=#{@id}"));
+		$content = Nokogiri::HTML(open("https://newlms.magtu.ru/mod/folder/view.php?id=#{@id}"))
 		rescue OpenURI::HTTPError => error
 			if error
-				errorFunc();
-				return nil;
+				errorFunc()
+				return nil
 			end
 		end
 
-		@lenghtArrayCSS = $content.css('.fp-filename').children.length;
+		@lenghtArrayCSS = $content.css('.fp-filename').children.length
 
-		(0...@lenghtArrayCSS).each do |i| 
+		(0...@lenghtArrayCSS).each do |i|
 			@ContentCss = $content.css('.fp-filename').children[i].text
 			if /\W{1,4}\-\d{2}\-\d{1}.xlsx$/.match?(@ContentCss)
-				$arrayGroupses.push(@ContentCss.delete('.xlsx'));
+				$arrayGroupses.push(@ContentCss.delete('.xlsx'))
 			elsif /\W{1,4}\-\d{2}\-\d{1} изм. с \d{2}.\d{2}.\d{2}.xlsx$/.match?(@ContentCss)
-				@str = @ContentCss.split[0];
-				$arrayGroupses.push(@str);
+				@str = @ContentCss.split[0]
+				$arrayGroupses.push(@str)
 			end
 		end
-			@id = @id + 1;
+			@id = @id + 1
 	end
 
 	@keyboards = $arrayGroupses.map do |arr|
 		Telegram::Bot::Types::InlineKeyboardButton.new(text: arr, callback_data: arr)
 	end
 	$selecteGroup =  Telegram::Bot::Types::InlineKeyboardMarkup.new(
-	                                    inline_keyboard: @keyboards);
+	                                    inline_keyboard: @keyboards)
 end
 
 def checkExistGroup(bot, message)
 	$db = Mysql2::Client.new(:host => "eu-cdbr-west-02.cleardb.net", :username => ENV["login"],
-		                     :password => ENV["password"]);
-	@userHash = Hash.new;
+		                     :password => ENV["password"])
+	@userHash = Hash.new
 	@results = $db.query("SELECT * FROM heroku_378417f804fd0eb.`user_table_group`
-												WHERE user_id = #{message.chat.id}");
-	@userHash = @results.each[0];
-	if @userHash != nil 
-		return @userHash["group_name"];
+												WHERE user_id = #{message.chat.id}")
+	@userHash = @results.each[0]
+	if @userHash != nil
+		return @userHash["group_name"]
 	else
-		createArrayGroups();
+		createArrayGroups()
 		if $selecteGroup != nil
-					bot.api.send_message(chat_id: message.chat.id, 
-		                     text: 'Бот не знает вашей группы, выберите её из списка.', 
-		                     reply_markup: $selecteGroup);
+					bot.api.send_message(chat_id: message.chat.id,
+		                     text: 'Бот не знает вашей группы, выберите её из списка.',
+		                     reply_markup: $selecteGroup)
 		end
 	end
 end
--- bot/src/ScheduleParsing.rb
+++ bot/src/ScheduleParsing.rb
@@ -1,81 +1,81 @@
 require 'roo'
 require './bot/src/CheckSubsLesson'
-$countY = 3;
+$countY = 3
 
 def funcListPars(day, groupId, groupTitle)
-@parsArray = [];     
-@mainArray = [];
-$countX = 1;
+@parsArray = []
+@mainArray = []
+$countX = 1
 
 CheckBaseAgendaExist(groupTitle)
-$xlsx = Roo::Excelx.new("./bot/xlsx/BaseAgendaFiles/#{groupTitle}.xlsx");
+$xlsx = Roo::Excelx.new("./bot/xlsx/BaseAgendaFiles/#{groupTitle}.xlsx")
 
 $xlsx.each_row_streaming.to_a.flatten.find do |row|
-	if row.inspect.include?(day.title); 
-		@mainArray.push(row);
-		@NumberOne = @mainArray[0];
-		@numberTwo = @mainArray[1];
+	if row.inspect.include?(day.title)
+		@mainArray.push(row)
+		@NumberOne = @mainArray[0]
+		@numberTwo = @mainArray[1]
 	end
 end
 
-if Time.now.strftime("%U").to_i.even? == false;
-	@currentWeek = @NumberOne;
+if Time.now.strftime("%U").to_i.even? == false
+	@currentWeek = @NumberOne
 else
-	@currentWeek = @numberTwo;
+	@currentWeek = @numberTwo
 end
-      while 
+      while
          $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + 1) != nil ||
          $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + 3) != nil
          if groupId == 1
-         	@lessonRoom = nil;
+         	@lessonRoom = nil
          	if $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + 1) != nil
          		if $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 2) != nil
          			@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 2)
          		else
-         			@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 4);
+         			@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 4)
          		end
-         		@lessonCount = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1]);
-         		@lessonTeacher = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 1);
-         		@lessonTitle = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + 1);
-         		@class = Hash.new;
-         		@class = {:room => @lessonRoom, :count => @lessonCount, :teacher => @lessonTeacher, :title => @lessonTitle};
-         		@parsArray.push(@class);
+         		@lessonCount = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1])
+         		@lessonTeacher = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 1)
+         		@lessonTitle = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + 1)
+         		@class = Hash.new
+         		@class = {:room => @lessonRoom, :count => @lessonCount, :teacher => @lessonTeacher, :title => @lessonTitle}
+         		@parsArray.push(@class)
          	end
          elsif groupId == 2
-         		@class = Hash.new;
+         		@class = Hash.new
            if $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + $countY) == nil &&
              	$xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + $countY + 1) != nil
-             	@lessonTitle = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + $countY - 2);
-         			@lessonCount = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1]);
-         			@lessonTeacher = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 1);
-         			@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 4);
-	         		@class = {:room => @lessonRoom, :count => @lessonCount, :teacher => @lessonTeacher, :title => @lessonTitle};
+             	@lessonTitle = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + $countY - 2)
+         			@lessonCount = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1])
+         			@lessonTeacher = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 1)
+         			@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + 4)
+	         		@class = {:room => @lessonRoom, :count => @lessonCount, :teacher => @lessonTeacher, :title => @lessonTitle}
              	@parsArray.push(@class)
-            else 
-            @lessonTitle = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + $countY);
-         		@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + $countY + 1);
-         		@lessonTeacher = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + $countY);
-						@lessonCount = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1]);
-	         	@class = {:room => @lessonRoom, :count => @lessonCount, :teacher => @lessonTeacher, :title => @lessonTitle};
+            else
+            @lessonTitle = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1] + $countY)
+         		@lessonRoom = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + $countY + 1)
+         		@lessonTeacher = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX + 1, @currentWeek.coordinate[1] + $countY)
+						@lessonCount = $xlsx.sheet(0).cell(@currentWeek.coordinate[0] + $countX, @currentWeek.coordinate[1])
+	         	@class = {:room => @lessonRoom, :count => @lessonCount, :teacher => @lessonTeacher, :title => @lessonTitle}
 
-            @parsArray.push(@class);
+            @parsArray.push(@class)
             end
          end
-         $countX = $countX + 2;
+         $countX = $countX + 2
       end
 			if File.exist?('./bot/xlsx/changeAgenda.xlsx')
 				subsLessonFunc(groupTitle).each do |lesson|
-					if lesson[:dayTitle] == day.title;
-							@parsArray[lesson[:count] - 1] = lesson;
+					if lesson[:dayTitle] == day.title
+							@parsArray[lesson[:count] - 1] = lesson
 					end
 				end
 			end
-			@string = '';
+			@string = ''
 			@parsArray.each do |less|
 				if less[:title] != nil
-					@string =  @string + "#{less[:count]}. #{less[:title]} #{less[:teacher]} #{less[:room]} " + "\n";
+					@string =  @string + "#{less[:count]}. #{less[:title]} #{less[:teacher]} #{less[:room]} " + "\n"
 				end
 		end
 		@poststring = "Расписание группы: #{groupTitle} на #{day.subtitle}: \n\n"
-	return @poststring + @string;
+	return @poststring + @string
 end
